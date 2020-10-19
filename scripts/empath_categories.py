1cc54f0f4349384bd1e5b7a23652dfc0583e6d6a
Mikko Roimaa <mikko.roimaa@bittium.com>
2020-10-19 21:22:24 +0300

    Upadated empath category script to used options. Uploaded empath data.

--- scripts/empath_categories.py
+++ scripts/empath_categories.py
@@ -1,31 +1,29 @@
 import csv
 import sys
-from empath import Empath
+import getopt
 import time
-
-file_name = sys.argv[1]
-review_col = sys.argv[2]
+from empath import Empath
 
 # This module runs Empath category analysis on a text in a single column in .csv file.
 # The first row of the input file is expected to be header.
-# Results are written in a new file empath_result.txt which contains
+# Results are written in a new file which contains
 # the input data appended with non-zero categories.
 #
-# Usage: python empath_categories.py [file] [column]
+# Usage: python empath_categories.py.py -i [in_file] -o [out_file] -c [column]
 
-with open(file_name) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-    review_col = int(review_col)
-
-    result = open('empath_result.txt', mode='w')
+def process_csv(in_file, out_file, column):
+    start_time = time.time()
+    
+    f = open(in_file, "r") 
+    csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+    
+    result = open(out_file, mode='w')
     csv_writer = csv.writer(result)
     
+    review_col = int(column)
     empath_column = 'categories'
-    
     lexicon = Empath()
-    
     line_count = 0
-    start_time = time.time()
     
     for row in csv_reader:
         if line_count == 0:
@@ -48,3 +46,23 @@ with open(file_name) as csv_file:
             line_count += 1
 
     print('Processed', line_count, 'rows in', "{:.2f}".format(time.time() - start_time), 'seconds.')
+    
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, 'i:o:c:')
+
+input_file = ""
+output_file = ""
+column = ""
+
+for opt in opts:
+    if opt[0] == '-i':
+        input_file = opt[1]
+    elif opt[0] == '-o':
+        output_file = opt[1]
+    elif opt[0] == '-c':
+        column = opt[1]
+
+if input_file == "" or output_file == "" or column == "":
+    print("Usage: python empath_categories.py.py -i [in_file] -o [out_file] -c [column]")
+else:
+    process_csv(input_file, output_file, column)
