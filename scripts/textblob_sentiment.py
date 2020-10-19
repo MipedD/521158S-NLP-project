2d201b5e87b30859ff26026ee6ffc5ff6f72e70f
Mikko Roimaa <mikko.roimaa@bittium.com>
2020-10-19 21:29:59 +0300

    Updated textblob script. Uploaded textblob results.

--- scripts/textblob_sentiment.py
+++ scripts/textblob_sentiment.py
@@ -1,26 +1,27 @@
 import csv
 import sys
-from textblob import TextBlob
+import getopt
 import time
-
-file_name = sys.argv[1]
-review_col = sys.argv[2]
+from textblob import TextBlob
 
 # This module runs TextBlob sentiment analysis on a text in a single column in .csv file.
 # The first row of the input file is expected to be header.
-# Results are written in a new file textblob_result.txt which contains
+# Results are written in a new file which contains
 # the input data appended with polarity and subjectivity columns.
 #
-# Usage: python textblob_sentiment.py [file] [column]
+# Usage: Usage: python textblob_sentiment.py -i [in_file] -o [out_file] -c [col]
 
-with open(file_name) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-    review_col = int(review_col)
-
-    result = open('textblob_result.txt', mode='w')
-    csv_writer = csv.writer(result)
-    line_count = 0
+def process_csv(in_file, out_file, column):
     start_time = time.time()
+    
+    f = open(in_file, "r") 
+    csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+    
+    result = open(out_file, mode='w')
+    csv_writer = csv.writer(result)
+    
+    review_col = int(column)
+    line_count = 0
  
     for row in csv_reader:
         if line_count == 0:
@@ -39,3 +40,23 @@ with open(file_name) as csv_file:
             line_count += 1
 
     print('Processed', line_count, 'rows in', "{:.2f}".format(time.time() - start_time), 'seconds.')
+    
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, 'i:o:c:')
+
+input_file = ""
+output_file = ""
+column = ""
+
+for opt in opts:
+    if opt[0] == '-i':
+        input_file = opt[1]
+    elif opt[0] == '-o':
+        output_file = opt[1]
+    elif opt[0] == '-c':
+        column = opt[1]
+
+if input_file == "" or output_file == "" or column == "":
+    print("Usage: python textblob_sentiment.py -i [in_file] -o [out_file] -c [col]")
+else:
+    process_csv(input_file, output_file, column)
