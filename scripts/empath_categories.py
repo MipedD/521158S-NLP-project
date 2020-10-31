dc0c41a659044906046464379848a636e56fea3f
Miika Pernu <pernumiika92@gmail.com>
2020-10-31 16:28:38 +0200

    Encoding, error handling and an option to append to input file

--- scripts/empath_categories.py
+++ scripts/empath_categories.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import getopt
 import time
+import pandas as pd
 from empath import Empath
 
 # This module runs Empath category analysis on a text in a single column in .csv file.
@@ -14,10 +15,10 @@ from empath import Empath
 def process_csv(in_file, out_file, column):
     start_time = time.time()
     
-    f = open(in_file, "r") 
+    f = open(in_file, "r", encoding='utf-8')
     csv_reader = csv.reader(f, delimiter=',', quotechar='"')
     
-    result = open(out_file, mode='w')
+    result = open(out_file, mode='w', encoding='utf-8')
     csv_writer = csv.writer(result)
     
     review_col = int(column)
@@ -35,9 +36,10 @@ def process_csv(in_file, out_file, column):
             review = row[review_col]
             categories = lexicon.analyze(review, normalize=True)
             trimmed_categories = dict()
-            for category, score in categories.items():
-                if score != 0:
-                    trimmed_categories[category] = score
+            if categories:
+                for category, score in categories.items():
+                    if score != 0:
+                        trimmed_categories[category] = score
             
             row.append(trimmed_categories)
  
@@ -46,13 +48,17 @@ def process_csv(in_file, out_file, column):
             line_count += 1
 
     print('Processed', line_count, 'rows in', "{:.2f}".format(time.time() - start_time), 'seconds.')
-    
-argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, 'i:o:c:')
 
 input_file = ""
 output_file = ""
 column = ""
+append_to_input = False
+
+argv = sys.argv[1:]
+if argv.index('-a') != -1:
+    append_to_input = True
+    argv.pop(argv.index('-a'))
+opts, args = getopt.getopt(argv, 'i:o:c:')
 
 for opt in opts:
     if opt[0] == '-i':
@@ -65,4 +71,9 @@ for opt in opts:
 if input_file == "" or output_file == "" or column == "":
     print("Usage: python empath_categories.py.py -i [in_file] -o [out_file] -c [column]")
 else:
-    process_csv(input_file, output_file, column)
+    #process_csv(input_file, output_file, column)
+    pass
+    if append_to_input:
+        print("Appending results to input file.")
+        data = pd.read_csv(output_file, sep=',', encoding='utf-8')
+        data.to_csv(input_file, sep=',', encoding='utf-8')
