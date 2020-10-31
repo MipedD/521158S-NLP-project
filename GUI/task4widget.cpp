3b08e15696d8faabe78c9604ccc1f4e832e629c7
Miika Pernu <pernumiika92@gmail.com>
2020-10-31 22:18:13 +0200

    Description for task 4

--- GUI/task4widget.cpp
+++ GUI/task4widget.cpp
@@ -3,7 +3,14 @@
 Task4Widget::Task4Widget(QWidget *parent, const QString &taskName)
     : TaskWidget(parent, taskName)
 {
+    const QString description =
+            "This step is about finding lexical categories within each review. <a href=https://github.com/Ejhfast/empath-client>Empath</a> "
+            "is used for finding the categories and empath output will be saved to the database."
 
+            "<br><br>"
+
+            "<b>Please note</b>: running the script may take up to few minutes. The amount of data processed is significant.";
+    setDescription(description);
 }
 
 void Task4Widget::doExecuteTask()
