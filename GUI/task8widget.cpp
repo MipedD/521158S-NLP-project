ef3ea81026c7294f2200fc4fd350ceb9de3dee43
Miika Pernu <pernumiika92@gmail.com>
2020-11-07 12:20:10 +0200

    Added description for task 8

--- GUI/task8widget.cpp
+++ GUI/task8widget.cpp
@@ -3,7 +3,20 @@
 Task8Widget::Task8Widget(QWidget *parent, const QString &taskName)
     : TaskWidget(parent, taskName)
 {
+    const QString description =
+            "In task 8 the hypothesis that the presence of a given named-entity "
+            "within a review would entail positive or negative sentiment is tested. "
+            "This is done by checking whether some category of named-entities "
+            "appears more frequently either in positive or negative reviews. "
+            "Again, negative reviews are considered those with user rating less or equal to "
+            "two and positive when the rating is equal or greater to 4 (on scale 1-5).";
 
+    setDescription(description);
+
+    QStringList requirements;
+    requirements << "Preparation step completed";
+    requirements << "All previous steps";
+    setRequirements(requirements);
 }
 
 void Task8Widget::doExecuteTask()
